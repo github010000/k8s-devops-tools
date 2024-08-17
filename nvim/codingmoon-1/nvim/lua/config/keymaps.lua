9a71805235aee9e040191ff641dda17f477a8866
K010k <youngmannkim@gmail.com>
2024-08-17 23:41:05 +0000

    Modify a jenkins yaml for fusion vm and Init a nvim settings with codingmoon.

---
+++ nvim/codingmoon-1/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+local mapKey = require("utils.keyMapper").mapKey
+
+-- Neotree toggle
+mapKey('<leader>e', ':Neotree toggle<cr>')
+
+-- pane navigation
+mapKey('<C-h>', '<C-w>h')
+mapKey('<C-l>', '<C-w>l')
+mapKey('<C-j>', '<C-w>j')
+mapKey('<C-k>', '<C-w>k')
+
+-- clear search highlight
+mapKey('<leader>h', ':nohlsearch<cr>')
